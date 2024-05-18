0147d78beef0175d79d6db9b7ea8174dfed41968
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2024-05-18 10:16:50 +0530

    Add Smart Attendance System project

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,28 +1,31 @@
 import React from "react";
-
 import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
 export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, source },
+  project: { title, imageSrc, description, skills, demo, source, video },
 }) => {
   return (
     <div className={styles.container}>
-      <img
+      {imageSrc && (<img
         src={getImageUrl(imageSrc)}
         alt={`Image of ${title}`}
         className={styles.image}
-      />
+      />)}
+      {video && (
+        <video width="297" height="199" controls className={styles.video}>
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      )}
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
       <ul className={styles.skills}>
-        {skills.map((skill, id) => {
-          return (
-            <li key={id} className={styles.skill}>
-              {skill}
-            </li>
-          );
-        })}
+        {skills.map((skill, id) => (
+          <li key={id} className={styles.skill}>
+            {skill}
+          </li>
+        ))}
       </ul>
       <div className={styles.links}>
         <a href={demo} className={styles.link}>
